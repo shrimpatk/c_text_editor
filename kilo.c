5e23c06a5851c1e4499c8db7a165359cd857d553
shrimpatk <mikey.swx@gmail.com>
2024-09-23 13:02:08 +0700

    Implement terminal size using ioctl() and fallback function and basic screen drawing

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -13,7 +14,14 @@
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+    int screen_rows;
+    int screen_cols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
+
 
 /*** terminal ***/
 
@@ -27,15 +35,15 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
@@ -59,10 +67,65 @@ char editorReadKey() {
     return c;
 }
 
+// Fallback when ioctl() didn't work as expected
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    // n command => query device status report
+    // 6 argument => ask for cursor position 
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    // String will always end with \0
+    buf[i] = '\0';
+
+    // check if it contain escape sequence
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    // read from &buf[2] and then parse it to rows and cols
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        editorReadKey();
+        // if ioctl() failed to get winsize use getCursorPosition() as a fallback
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < E.screen_rows; y++) {
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screen_rows - 1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
+    }
+}
 
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
@@ -73,6 +136,9 @@ void editorProcessKeypress() {
 
     switch (c){
         case CRTL_KEY('q'): // ASCII 17 | 0x11
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+
             exit(0);
             break;
     }
@@ -80,8 +146,13 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screen_rows, &E.screen_cols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
