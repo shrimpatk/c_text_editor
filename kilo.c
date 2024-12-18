47d6726bbf6364c3c9211cf8a7f4d5904e89b77d
shrimpatk <mikey.swx@gmail.com>
2024-12-18 13:51:11 +0700

    Implement search functionality with live incremental search

--- kilo.c
+++ kilo.c
@@ -68,7 +68,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 
@@ -208,6 +208,19 @@ int editorRowCursorXToRenderX(editor_row *row, int cursor_x) {
     render_x++;
   }
   return render_x;
+}
+
+int editorRowRxToCx(editor_row *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t') 
+      cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+    cur_rx++;
+
+    if (cur_rx > rx) return cx;
+  }
+  return cx;
 }
 
 // First loop is find all the tabs in a row
@@ -386,7 +399,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
   if (E.filename == NULL) {
-    E.filename = editorPrompt("Save as: %s");
+    E.filename = editorPrompt("Save as: %s", NULL);
     if (E.filename == NULL) {
       editorSetStatusMessage("Save aborted");
       return;
@@ -412,6 +425,64 @@ void editorSave() {
 
   free(buf);
   editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+
+void editorFindCallback(char *query, int key) {
+  static int last_match = -1;
+  static int direction = 1;
+
+  if (key == '\r' || key == '\x1b') {
+    last_match = -1;
+    direction = 1;
+    return;
+  } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    direction = 1;
+  } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    direction = -1;
+  } else {
+    last_match = -1;
+    direction = 1;
+  }
+
+  if (last_match == -1) direction = 1;
+  int current = last_match;
+  int i;
+  for (i = 0; i < E.numrows; i++) {
+    current += direction;
+    if (current == -1) current = E.numrows - 1;
+    else if (current == E.numrows) current = 0;
+
+    editor_row *row = &E.row[current];
+    char *match = strstr(row->render, query);
+    if (match) {
+      last_match = current;
+      E.cursor_y = current;
+      E.cursor_x = editorRowRxToCx(row, match - row->render);
+      E.row_offset = E.numrows;
+      break;
+    }
+  }
+}
+
+void editorFind() {
+  int saved_cx = E.cursor_x;
+  int saved_cy = E.cursor_y;
+  int saved_col_offset = E.col_offset;
+  int saved_row_offset = E.row_offset;
+
+  char *query = editorPrompt("Search: %s (Use ESC|Arrows|Enter)",
+                             editorFindCallback);
+
+  if (query) {
+    free(query);
+  } else {
+    E.cursor_x = saved_cx;
+    E.cursor_y = saved_cy;
+    E.col_offset = saved_col_offset;
+    E.row_offset = saved_row_offset;
+  }
 }
 
 /*** Append Buffer ***/
@@ -583,7 +654,7 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /*** input ***/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
   size_t bufsize = 128;
   char *buf = malloc(bufsize);
 
@@ -599,11 +670,13 @@ char *editorPrompt(char *prompt) {
       if (buflen != 0) buf[--buflen] = '\0';
     } else if (c == '\x1b') {
       editorSetStatusMessage("");
+      if (callback) callback(buf, c);
       free(buf);
       return NULL;
     } else if (c == '\r') {
       if (buflen != 0) {
         editorSetStatusMessage("");
+        if (callback) callback(buf, c);
         return buf;
       }
     } else if (!iscntrl(c) && c < 128) {
@@ -614,6 +687,8 @@ char *editorPrompt(char *prompt) {
       buf[buflen++] = c;
       buf[buflen] = '\0';
     }
+
+    if (callback) callback(buf, c);
   }
 }
 
@@ -693,6 +768,10 @@ void editorProcessKeypress() {
         E.cursor_x = E.row[E.cursor_y].size;
       break;
 
+    case CRTL_KEY('f'):
+      editorFind();
+      break;
+
     case BACKSPACE:
     case CRTL_KEY('h'):
     case DEL_KEY:
@@ -760,7 +839,8 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: Ctrl-S = save | Crtl-Q = quit");
+  editorSetStatusMessage(
+    "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
   while (1) {
     editorRefreshScreen();
