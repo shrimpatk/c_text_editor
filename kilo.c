2020953013fa11ce73663e95bd7e09034c1abe00
shrimpatk <mikey.swx@gmail.com>
2024-12-21 14:34:53 +0700

    Implement digit syntax highlighting

--- kilo.c
+++ kilo.c
@@ -37,6 +37,12 @@ enum editorKey {
   PAGE_DOWN
 };
 
+enum editorHighlight {
+  HL_NORMAL = 0,
+  HL_NUMBER,
+  HL_MATCH
+};
+
 /*** data ***/
 
 typedef struct editor_row {
@@ -44,6 +50,7 @@ typedef struct editor_row {
   int rsize;
   char *chars;
   char *render;
+  unsigned char *hl;
 } editor_row;
 
 struct editorConfig {
@@ -195,6 +202,44 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
+/*** syntax highlighting ***/
+
+int is_seperator(int c) {
+  return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
+void editorUpdateSyntax(editor_row *row) {
+  row->hl = realloc(row->hl, row->rsize);
+  memset(row->hl, HL_NORMAL, row->rsize);
+
+  int prev_separator = 1;
+
+  int i = 0;
+  while (i < row->rsize) {
+    char c = row->render[i];
+    unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if ((isdigit(c) && (prev_separator || prev_hl == HL_NUMBER)) || 
+        (c == '.' && prev_hl == HL_NUMBER)) {
+      row->hl[i] = HL_NUMBER;
+      i++;
+      prev_separator = 0;
+      continue;
+    }
+
+    prev_separator = is_seperator(c);
+    i++;
+  }
+}
+
+int editorSyntaxToColor(int hl) {
+  switch (hl) {
+    case HL_NUMBER: return 31;
+    case HL_MATCH: return 34;
+    default: return 37;
+  }
+}
+
 /*** Row Operations ***/
 
 // Convert chars index to render index
@@ -250,6 +295,8 @@ void editorUpdateRow(editor_row *row) {
   }
   row->render[idx] = '\0';
   row->rsize = idx;
+
+  editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at, char *s, size_t len) {
@@ -265,6 +312,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 
   E.row[at].rsize = 0;
   E.row[at].render = NULL;
+  E.row[at].hl = NULL;
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
@@ -274,6 +322,7 @@ void editorInsertRow(int at, char *s, size_t len) {
 void editorFreeRow(editor_row *row) {
   free(row->render);
   free(row->chars);
+  free(row->hl);
 }
 
 void editorDelRow(int at) {
@@ -433,6 +482,15 @@ void editorFindCallback(char *query, int key) {
   static int last_match = -1;
   static int direction = 1;
 
+  static int saved_hl_line;
+  static char *saved_hl = NULL;
+
+  if (saved_hl) {
+    memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+    free(saved_hl);
+    saved_hl = NULL;
+  }
+
   if (key == '\r' || key == '\x1b') {
     last_match = -1;
     direction = 1;
@@ -461,6 +519,11 @@ void editorFindCallback(char *query, int key) {
       E.cursor_y = current;
       E.cursor_x = editorRowRxToCx(row, match - row->render);
       E.row_offset = E.numrows;
+
+      saved_hl_line = current;
+      saved_hl = malloc(row->rsize);
+      memcpy(saved_hl, row->hl, row->rsize);
+      memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
       break;
     }
   }
@@ -573,7 +636,29 @@ void editorDrawRows(struct abuf *ab) {
       int len = E.row[file_row].rsize - E.col_offset;
       if (len < 0) len = 0;
       if (len > E.screen_cols) len = E.screen_cols;
-      abAppend(ab, &E.row[file_row].render[E.col_offset], len);
+      char *c = &E.row[file_row].render[E.col_offset];
+      unsigned char *hl = &E.row[file_row].hl[E.col_offset];
+      int current_color = -1;
+      int j;
+      for (j = 0; j < len; j++) {
+        if (hl[j] == HL_NORMAL) {
+          if (current_color != -1) {
+            abAppend(ab, "\x1b[39m", 5);
+            current_color = -1;
+          }
+          abAppend(ab, &c[j], 1);
+        } else {
+          int color = editorSyntaxToColor(hl[j]);
+          if (color != current_color) {
+            current_color = color;
+            char buf[16];
+            int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", color);
+            abAppend(ab, buf, clen);
+          }
+          abAppend(ab, &c[j], 1);
+        }
+      }
+      abAppend(ab, "\x1b[39m", 5);      
     }
 
     // K is delete in line and 0 args is erase right of cursor
